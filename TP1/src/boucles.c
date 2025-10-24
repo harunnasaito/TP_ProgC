d1253ebf748d288d005d10db8efb43c6972292ff
harunnasaito <lunahilaire@gmail.com>
2025-10-24 11:07:04 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,7 +1,30 @@
+##Boucle For 
+
+##include <stdio.h>
+
+##int main() {
+    ##int compteur = 5; 
+
+    ##for (int i = 1; i <= compteur; i++) {
+        ##for (int j = 1; j <= i; j++) {
+            ##if (j % 2 == 0) {
+                ##printf(##"# ");
+            ##} ##else {
+                ##printf(##"* ");
+            ##}
+       ##}
+        ##printf(##"\n");
+    ##}
+
+    ##return 0;
+##}
+
+##Boucle While 
+
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // modifier la taille du triangle
+    int compteur = 5; 
     int i = 1;
 
     while (i <= compteur) {
@@ -19,3 +42,4 @@ int main() {
 
     return 0;
 }
+
