f6392b807e3c00dde51d84a62a7bd836ad35c935
harunnasaito <lunahilaire@gmail.com>
2025-10-24 11:26:59 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;    
+    int b = 3;   
+    int resultat = 1; 
+
+   
+    for(int i = 0; i < b; i++) {
+        resultat *= a;
+    }
+
+
+    printf("%d élevé à la puissance %d = %d\n", a, b, resultat);
+
+    return 0;
+}
