dfe2fafa646c4717ea69969955cf5fdc5f1500dc
harunnasaito <lunahilaire@gmail.com>
2025-12-02 10:25:52 +0100

    Update chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -1,77 +1,37 @@
 #include <stdio.h>
 
+int main() {
+    char chaine1[50] = "Hello";
+    char chaine2[50] = " World!";
+    char copie[50];
+    char concat[100];
+    int i = 0, j = 0, longueur = 0;
 
-int longueur(const char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        i++;
-    }
-    return i;
-}
-
-
-void copier(char *dest, const char *src) {
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0'; 
-}
-
-
-void concatener(char *dest, const char *src) {
-    int i = 0;
-    int j = 0;
-
-    =
-    while (dest[i] != '\0') {
-        i++;
+    while (chaine1[longueur] != '\0') {
+        longueur++;
     }
 
-    
-    while (src[j] != '\0') {
-        dest[i] = src[j];
+    while (chaine1[i] != '\0') {
+        copie[i] = chaine1[i];
+        i++;
+    }
+    copie[i] = '\0';
+
+    i = 0;
+    while (chaine1[i] != '\0') {
+        concat[i] = chaine1[i];
+        i++;
+    }
+    while (chaine2[j] != '\0') {
+        concat[i] = chaine2[j];
         i++;
         j++;
     }
+    concat[i] = '\0';
 
-    dest[i] = '\0';
-}
-
-int main() {
-    char chaine1[100];
-    char chaine2[100];
-    char copie[100];
-    char concat[200];
-
-    printf("Entrez la première chaîne : ");
-    fgets(chaine1, sizeof(chaine1), stdin);
-
-    printf("Entrez la deuxième chaîne : ");
-    fgets(chaine2, sizeof(chaine2), stdin);
-
-    
-    if (chaine1[longueur(chaine1)-1] == '\n')
-        chaine1[longueur(chaine1)-1] = '\0';
-    if (chaine2[longueur(chaine2)-1] == '\n')
-        chaine2[longueur(chaine2)-1] = '\0';
-
-    
-    int len1 = longueur(chaine1);
-    int len2 = longueur(chaine2);
-
-    printf("\nLongueur de la première chaîne : %d\n", len1);
-    printf("Longueur de la deuxième chaîne : %d\n", len2);
-
-    
-    copier(copie, chaine1);
-    printf("\nCopie de la première chaîne : %s\n", copie);
-
-    
-    copier(concat, chaine1);  
-    concatener(concat, chaine2); 
-    printf("Concaténation des deux chaînes : %s\n", concat);
+    printf("Longueur de chaine1 : %d\n", longueur);
+    printf("Copie de chaine1 : %s\n", copie);
+    printf("Concaténation : %s\n", concat);
 
     return 0;
 }
