dc04a26e9359214eda841f40918be45a96ad2696
harunnasaito <lunahilaire@gmail.com>
2025-12-02 10:42:10 +0100

    Update couleurs.c

---
+++ TP2/src/couleurs.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct Couleur {
+    unsigned char r; 
+    unsigned char g; 
+    unsigned char b; 
+    unsigned char a; 
+};
+
+int main() {
+    
+    struct Couleur couleurs[10] = {
+        {0xef, 0x78, 0x12, 0xff},
+        {0x2c, 0xc8, 0x64, 0xff},
+        {0x00, 0x80, 0xff, 0xff},
+        {0xff, 0x00, 0x00, 0xff},
+        {0x00, 0xff, 0x00, 0xff},
+        {0x00, 0x00, 0xff, 0xff},
+        {0xff, 0xff, 0x00, 0xff},
+        {0xff, 0x00, 0xff, 0xff},
+        {0x00, 0xff, 0xff, 0xff},
+        {0x80, 0x80, 0x80, 0xff}
+    };
+
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Couleur %d :\n", i + 1);
+        printf("Rouge  : %d\n", couleurs[i].r);
+        printf("Vert   : %d\n", couleurs[i].g);
+        printf("Bleu   : %d\n", couleurs[i].b);
+        printf("Alpha  : %d\n\n", couleurs[i].a);
+    }
+
+    return 0;
+}
